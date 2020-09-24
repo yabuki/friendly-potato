c6101de5b497b72a95c32079a953b6cb75436298
Yukiharu YABUKI <yabuki@netfort.gr.jp>
2020-09-24 19:12:38 +0900

    Added helper script

---
+++ new-article.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+echo $1
+arg1="$1"
+charcnt=${#arg1}
+echo $charcnt
+# 2020-09-24- 11characters
+# 50-11=39
+if [ 39 -ge $charcnt ]; then
+    echo "doit"
+    npx zenn new:article --slug `date +%Y-%m-%d`-$1
+else
+    echo "Too long slug. Be more shorten"
+fi
+#npx zenn new:article --slug `date +%Y-%m-%d`-
